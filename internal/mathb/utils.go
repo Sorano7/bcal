bc359b1dfa24c29e74d212870e61df0517fc0976
Sorano7 <sorano27@outlook.com>
2026-01-25 18:01:23 -0500

    Rewrite util functions expecting big.Int

--- internal/mathb/utils.go
+++ internal/mathb/utils.go
@@ -1,5 +1,7 @@
 package mathb
 
+import "math/big"
+
 // Computes the GCD of two integers.
 func gcd(a, b int64) int64 {
 	for b != 0 {
@@ -9,47 +11,36 @@ func gcd(a, b int64) int64 {
 }
 
 // Computes the LCM of the provided intergers.
-func lcm(a, b int64, n ...int64) int64 {
-	res := (a / gcd(a, b)) * b
-	for i := range n {
-		res = lcm(res, n[i])
-	}
-	return res
+func lcm(a, b *big.Int) *big.Int {
+	g := new(big.Int).GCD(nil, nil, a, b)
+	t := new(big.Int).Div(a, g)
+	t.Abs(t)
+	t.Mul(t, b)
+	t.Abs(a)
+	return t
 }
 
-// Normalize the rational.
-func (n Rational) normalize() Rational {
-	d := gcd(n.num, n.denom)
-	n = Rational{n.num / d, n.denom / d, n.base}
-	if n.denom < 0 {
-		n = Rational{-n.num, -n.denom, n.base}
+// Mutates and normalize the rational.
+func (n *Rational) normalize() *Rational {
+	g := new(big.Int).GCD(nil, nil, n.num, n.denom)
+	n.num.Div(n.num, g)
+	n.denom.Div(n.denom, g)
+	if n.denom.Sign() < 0 {
+		n.num.Neg(n.num)
+		n.denom.Neg(n.denom)
 	}
 	return n
 }
 
-// Match the two rationals' denominator and return both rationals.
-func matchDenom(a, b Rational) (Rational, Rational) {
+// Mutates to match the two rationals' denominator and return both rationals.
+func matchDenom(a, b *Rational) {
 	d := lcm(a.denom, b.denom)
-	a = Rational{a.num * (d / a.denom), d, a.base}
-	b = Rational{b.num * (d / b.denom), d, b.base}
-	return a, b
-}
-
-// Return whether this rational terminates in its prefered base.
-func (n Rational) terminatesInBase() bool {
-	n = n.Abs()
-
-	g := gcd(n.num, n.denom)
-	n.denom /= g
-
-	for {
-		g := gcd(n.denom, n.base)
-		if g == 1 {
-			break
-		}
-		n.denom /= g
-	}
-	return n.denom == 1
+	tmp := new(big.Int)
+	a.num.Mul(a.num, tmp.Div(d, a.denom))
+	b.num.Mul(b.num, tmp.Div(d, b.denom))
+	a.denom = d
+	tmp.Set(d)
+	b.denom = tmp
 }
 
 // Integer power.
@@ -63,4 +54,36 @@ func intPow(a, b int64) int64 {
 		b /= 2
 	}
 	return x
+}
+
+// Compute the non-repeating and repeating part of this rational.
+func (n *Rational) splitFrac() ([]int64, []int64) {
+	tmp := *n
+	denom := tmp.denom
+
+	rem := tmp.Abs().num.Mod(tmp.num, denom)
+	if rem.Sign() == 0 {
+		return nil, nil
+	}
+
+	b := big.NewInt(n.base)
+	seen := make(map[string]int, 64)
+	digits := make([]int64, 0, 64)
+
+	t := new(big.Int)
+	for {
+		if rem.Sign() == 0 {
+			return digits, nil
+		}
+
+		key := string(rem.Bytes())
+		if split, ok := seen[key]; ok {
+			return digits[:split], digits[split:]
+		}
+		seen[key] = len(digits)
+
+		t.Mul(rem, b)
+		t.QuoRem(t, denom, rem)
+		digits = append(digits, t.Int64())
+	}
 }
