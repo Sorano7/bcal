2cf97ad5c269903dc34c1e266b094204e269e298
Sorano7 <sorano27@outlook.com>
2026-01-25 18:01:02 -0500

    Rewrite Rational struct and methods with *big.Int

--- internal/mathb/rational.go
+++ internal/mathb/rational.go
@@ -1,42 +1,29 @@
 package mathb
 
-import "fmt"
-
-var Zero Rational = newRational(0, 1, 10)
+import (
+	"fmt"
+	"math/big"
+)
 
 // Represents a base rational in normalized form.
 type Rational struct {
-	num   int64
-	denom int64
+	num   *big.Int
+	denom *big.Int
 	base  int64
 }
 
-// Get the numerator of the rational.
-func (n Rational) Num() int64 {
-	return n.num
+// Sets n's base to base.
+func (n *Rational) WithBase(base int64) *Rational {
+	n.base = base
+	return n
 }
 
-// Get the Denominator of the rational.
-func (n Rational) Denom() int64 {
-	return n.denom
-}
-
-// Get the (preferred) base of the rational.
-func (n Rational) Base() int64 {
-	return n.base
-}
-
-// Create a new rational with the provided base.
-func (n Rational) WithBase(base int64) Rational {
-	return newRational(n.num, n.denom, base)
-}
-
-// Constructs a new rational.
-func newRational(num, denom, base int64) Rational {
+// Allocates a new rational with num, denom, and base.
+func newRational(num, denom, base int64) *Rational {
 	if denom == 0 {
 		panic("Division by zero")
 	}
-	r := Rational{num, denom, base}
+	r := &Rational{big.NewInt(num), big.NewInt(denom), base}
 	return r.normalize()
 }
 
@@ -45,7 +32,7 @@ func newRational(num, denom, base int64) Rational {
 // I, N, R: the value of the integer, non-repeating, repeating parts.
 //
 // n, r: the number of digits of the non-repeating and repeating parts.
-func newRationalFromDec(I, N, R, n, r, base int64) Rational {
+func newRationalFromDec(I, N, R, n, r, base int64) *Rational {
 	var num, denom int64
 
 	if r == 0 {
@@ -59,74 +46,66 @@ func newRationalFromDec(I, N, R, n, r, base int64) Rational {
 	return newRational(num, denom, base)
 }
 
-// The absolute value of the rational.
-func (n Rational) Abs() Rational {
+// Sets n to the absolute value of n.
+func (n *Rational) Abs() *Rational {
 	if n.Sign() == -1 {
-		n.num = -n.num
+		n.num.Neg(n.num)
 	}
 	return n
 }
 
 // Compare this with another rational. Return 0 if equal, 1 if greater than,
 // -1 if less than.
-func (n Rational) Cmp(other Rational) int {
-	n, other = matchDenom(n, other)
-	switch {
-	case n.num == other.num:
-		return 0
-	case n.num > other.num:
-		return 1
-	default:
-		return -1
-	}
+func (n *Rational) Cmp(other *Rational) int {
+	matchDenom(n, other)
+	return n.num.Cmp(other.num)
 }
 
 // Get the sign of this rational.
-func (n Rational) Sign() int {
-	switch {
-	case n.num == 0:
-		return 0
-	case n.num > 0:
-		return 1
-	default:
-		return -1
-	}
+func (n *Rational) Sign() int {
+	return n.num.Sign()
 }
 
-// Add this to another rational.
-func (n Rational) Add(other Rational) Rational {
-	n, other = matchDenom(n, other)
-	n.num += other.num
+// Sets n to the result of n + other.
+func (n *Rational) Add(other *Rational) *Rational {
+	matchDenom(n, other)
+	n.num.Add(n.num, other.num)
 	return n.normalize()
 }
 
 // Split the rational into the quotient and remainder.
-func (n Rational) Divmod() (int64, int64) {
-	return n.num / n.denom, n.num % n.denom
+func (n *Rational) Divmod() (*big.Int, *big.Int) {
+	q := new(big.Int).Div(n.num, n.denom)
+	r := new(big.Int).Mod(n.num, n.denom)
+	return q, r
 }
 
-func (n Rational) Sub(other Rational) Rational {
-	n, other = matchDenom(n, other)
-	n.num -= other.num
+// Sets n to the result of n - other.
+func (n *Rational) Sub(other *Rational) *Rational {
+	matchDenom(n, other)
+	n.num.Sub(n.num, other.num)
 	return n.normalize()
 }
 
-func (n Rational) Mul(other Rational) Rational {
-	n.num *= other.num
-	n.denom *= other.denom
+// Sets n to the result of n * other.
+func (n *Rational) Mul(other *Rational) *Rational {
+	n.num.Mul(n.num, other.num)
+	n.denom.Mul(n.denom, other.denom)
 	return n.normalize()
 }
 
-func (n Rational) Div(other Rational) (Rational, error) {
-	if other.num == 0 {
-		return Rational{}, fmt.Errorf("Division by zero")
+// Sets n to the result of n / other.
+func (n *Rational) Div(other *Rational) (*Rational, error) {
+	if other.num.Sign() == 0 {
+		return nil, fmt.Errorf("Division by zero")
 	}
-	n.num *= other.denom
-	n.denom *= other.num
+	n.num.Mul(n.num, other.denom)
+	n.denom.Mul(n.denom, other.num)
 	return n.normalize(), nil
 }
 
-func (n Rational) Neg() Rational {
-	n.num = -n.num
+// Sets n to the result of -n.
+func (n *Rational) Neg() *Rational {
+	n.num.Neg(n.num)
 	return n
 }
