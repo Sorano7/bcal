4fd6c64be32e34e00989e8476e1d0adc6459c448
Sorano7 <sorano27@outlook.com>
2026-01-25 13:09:49 -0500

    Refactor

--- internal/mathb/rational_test.go
+++ internal/mathb/rational_test.go
@@ -17,36 +17,42 @@ func printExpr(a, b, result mathb.Rational) {
 	fmt.Printf("%s <op> %s = %s\n", a.RatString(), b.RatString(), result.RatString())
 }
 
-func TestParsing(t *testing.T) {
-	if _, err := mathb.FromString("100", 10); err != nil {
+func TestParsingDigits(t *testing.T) {
+	x, _ := mathb.ParseString("ABC", 16)
+	y, _ := mathb.ParseDigitList([]int64{10, 11, 12}, nil, nil, 16)
+	assertEqual(x, y, t)
+}
+
+func TestParsingString(t *testing.T) {
+	if _, err := mathb.ParseString("100", 10); err != nil {
 		t.Fatal(err)
 	}
-	if _, err := mathb.FromString("1234", 2); err == nil {
+	if _, err := mathb.ParseString("1234", 2); err == nil {
 		t.Fatal(err)
 	}
-	if _, err := mathb.FromString("&&&", 60); err == nil {
+	if _, err := mathb.ParseString("&&&", 60); err == nil {
 		t.Fatal(err)
 	}
-	if _, err := mathb.FromString("ABC", 100); err == nil {
+	if _, err := mathb.ParseString("ABC", 100); err == nil {
 		t.Fatal(err)
 	}
 	str := "1AB24.AB5345"
-	x, _ := mathb.FromString(str, 12)
+	x, _ := mathb.ParseString(str, 12)
 	if x.String() != str {
 		t.Fatalf("want: %s, got: %s", str, x)
 	}
 }
 
 func TestAddInt(t *testing.T) {
-	x, _ := mathb.FromString("1A", 12)
-	y, _ := mathb.FromString("1", 12)
-	want, _ := mathb.FromString("1B", 12)
+	x, _ := mathb.ParseString("1A", 12)
+	y, _ := mathb.ParseString("1", 12)
+	want, _ := mathb.ParseString("1B", 12)
 	assertEqual(x.Add(y), want, t)
 }
 
 func TestAddFrac(t *testing.T) {
-	x, _ := mathb.FromString("1.A1", 12)
-	y, _ := mathb.FromString("2.3B", 12)
-	want, _ := mathb.FromString("4.2", 12)
+	x, _ := mathb.ParseString("1.A1", 12)
+	y, _ := mathb.ParseString("2.3B", 12)
+	want, _ := mathb.ParseString("4.2", 12)
 	assertEqual(x.Add(y), want, t)
 }
