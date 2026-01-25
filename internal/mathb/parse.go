a0dcbc305d46e7aaeab01217200ecf11edf6efe2
Sorano7 <sorano27@outlook.com>
2026-01-25 13:09:24 -0500

    Add parsing methods for digit lists

--- internal/mathb/parse.go
+++ internal/mathb/parse.go
@@ -6,8 +6,41 @@ import (
 	"strings"
 )
 
+// Parse a rational from lists of digits.
+func ParseDigitList(intPart, nonrep, rep []int64, base int64) (Rational, error) {
+	I, err := basebAcc(intPart, base)
+	if err != nil {
+		return Rational{}, err
+	}
+	N, err := basebAcc(nonrep, base)
+	if err != nil {
+		return Rational{}, err
+	}
+	R, err := basebAcc(rep, base)
+	if err != nil {
+		return Rational{}, err
+	}
+
+	n, r := int64(len(nonrep)), int64(len(rep))
+
+	return newRationalFromDec(I, N, R, n, r, base), nil
+}
+
+// Parse a rational from numerator and denominator with base respect.
+func ParseRational(num, denom string, base int64) (Rational, error) {
+	n, err := valueInBase(num, base)
+	if err != nil {
+		return Rational{}, err
+	}
+	d, err := valueInBase(denom, base)
+	if err != nil {
+		return Rational{}, err
+	}
+	return newRational(n, d, base), nil
+}
+
 // Parse a rational from parts.
-func FromParts(intPart, nonrep, rep string, base int64) (Rational, error) {
+func ParseParts(intPart, nonrep, rep string, base int64) (Rational, error) {
 	if base >= int64(len(Digits)) {
 		return Rational{}, fmt.Errorf("Base %d exceeds max representable base", base)
 	}
@@ -24,25 +57,14 @@ func FromParts(intPart, nonrep, rep string, base int64) (Rational, error) {
 		return Rational{}, err
 	}
 
-	n := len(nonrep)
-	r := len(rep)
+	n := int64(len(nonrep))
+	r := int64(len(rep))
 
-	var num, denom int64
-
-	if r == 0 {
-		denom = intPow(base, int64(n))
-		num = I*denom + int64(N)
-	} else {
-		x := (intPow(base, int64(r)) - 1)
-		denom = intPow(base, int64(n)) * x
-		num = I*denom + N*x + R
-	}
-
-	return newRational(num, denom, base), nil
+	return newRationalFromDec(I, N, R, n, r, base), nil
 }
 
 // Initialize a rational from a string.
-func FromString(lit string, base int64) (Rational, error) {
+func ParseString(lit string, base int64) (Rational, error) {
 	parts := strings.Split(lit, ".")
 	if len(parts) > 2 {
 		return Rational{}, fmt.Errorf("Invalid literal")
@@ -51,15 +73,45 @@ func FromString(lit string, base int64) (Rational, error) {
 	nonrep := ""
 	rep := ""
 	if len(parts) == 2 {
-		frac := parts[1]
-		regex := regexp.MustCompile(`\[[0-9A-Za-z]+\]`)
+		nonrep = parts[1]
+		regex := regexp.MustCompile(`\([0-9A-Za-z]+\)`)
 		match := regex.FindStringIndex(parts[1])
 		if match != nil {
-			rep = frac[match[0]:match[1]]
+			rep = nonrep[match[0]:match[1]]
+			nonrep = nonrep[:match[0]]
 		}
-		nonrep = frac[:match[0]]
 	}
-	return FromParts(intPart, nonrep, rep, base)
+	return ParseParts(intPart, nonrep, rep, base)
+}
+
+// Base-b accummulation.
+func basebAcc(digits []int64, base int64) (int64, error) {
+	if digits == nil {
+		return 0, nil
+	}
+	var v int64 = 0
+	for _, d := range digits {
+		if d >= base {
+			return 0, fmt.Errorf("Digit %d out of range for base %d", d, base)
+		}
+		v = v*base + d
+	}
+	return v, nil
+}
+
+func StringToList(digits string, base int64) ([]int64, error) {
+	if digits == "" {
+		return nil, nil
+	}
+	var out []int64
+	for _, c := range digits {
+		val, err := charToVal(c, base)
+		if err != nil {
+			return nil, err
+		}
+		out = append(out, val)
+	}
+	return out, nil
 }
 
 // Parse the value of a string in the given base.
@@ -67,15 +119,11 @@ func valueInBase(s string, base int64) (int64, error) {
 	if s == "" {
 		return 0, nil
 	}
-	var x int64 = 0
-	for _, c := range s {
-		val, err := charToVal(c, base)
-		if err != nil {
-			return 0, err
-		}
-		x = x*base + val
+	digits, err := StringToList(s, base)
+	if err != nil {
+		return 0, err
 	}
-	return int64(x), nil
+	return basebAcc(digits, base)
 }
 
 // Convert a character to digit value.
